6110742b624018fa47d0f72ff61ef565e6d06483
samuelkyama <91733138+samuelkyama@users.noreply.github.com>
2021-10-22 14:13:39 +0300

    Add files via upload

---
+++ relational operators.c
@@ -0,0 +1,14 @@
+// working of relational operators
+#include <stdio.h>
+
+int main()
+{
+	int a=5,b=10;
+	printf("Return value of(%d==%d)is %d\n",a,b,a==b);
+	printf("Return value of (%d!=%d)is %d\n",a,b,a!=b);
+	printf("Return value of (%d<%d)is %d\n",a,b,a<b);
+	printf("Return value of (%d<=%d)is %d\n",a,b,a<=b);
+	printf("Return value of (%d>%d)is %d\n",a,b,a>b);
+	printf("Return value of (%d>=%d)is %d\n",a,b,a>=b);
+	return 0;
+	}
