c19742e47259d528ebc636fc4bd28015641a678b
samuelkyama <91733138+samuelkyama@users.noreply.github.com>
2021-10-22 14:53:22 +0300

    Add files via upload

---
+++ Area of rectangle.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int w,l,Area;
+	printf("\nEnter value to w:");
+	scanf("%d",&w);
+	printf("\nEnter value to l:");
+	scanf("%d",&l);
+	Area=l*w;
+	printf("Area=%d",Area);
+	return 0;
+	}
